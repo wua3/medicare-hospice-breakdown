015d214a1cfbf78087a9a1972a8ea7bc907b06d4
wua3 <aileenwu24601@gmail.com>
2019-07-22 13:56:24 -0400

    Update text to next draft version

--- scripts/reporting.js
+++ scripts/reporting.js
@@ -1,7 +1,7 @@
 var dataset = [
-    ["Hospices", "Only requirement to report was in one specific, narrow circumstance"], 
-    ["Surveyors", "Until March 5, 2019, requirements were barely specified"], 
-    ["Beneficiaries & caregivers", "Instructions on how to report were extremely confusing and unclear"], 
+    ["Hospices", "only required to report to CMS in one narrow circumstance...", "...if allegation of harm + someone is furnishing services on behalf of the hospice + hospice has investigated and verified the allegation", "compare: at nursing facilities, all alleged (not just verified) violations must be reported"], 
+    ["Surveyors", "requirements were, until March 5, 2019, barely specified beyond 'immediate jeopardy'...", "...were required to report to local law enforcement only if hospice refused to report", "more specific guidance for surveys for nursing facilities"], 
+    ["Beneficiaries & caregivers", "instructions on how to do so were extremely confusing and unclear", "hospices not required to provide information on how to log a complaint and don’t always handle complaints properly", "information not easily accessible via CMS’s consumer website."], 
   ];
 
 
@@ -25,3 +25,12 @@ div.selectAll("div").append("p")
         return d[1];
     });
           
+        
+div.selectAll("div").append("li")
+    .text(function(d) {
+        return d[2];
+    });
+div.selectAll("div").append("li")
+    .text(function(d) {
+      return d[3];
+    });
